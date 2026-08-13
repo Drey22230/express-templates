b2f6d40e56ac781cdb545ae5b7ec41dea09d756a
Drey22230 <andriexenon@gmail.com>
2026-08-14 03:56:49 +0800

    adding dummy

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { errorMiddleware } from "./middlewares/error.middlewares.js";
+import { helloWorld } from "./modules/Hello World/hello.js";
+import { hi } from "./modules/Hi/hi.js";
 const app = express();
 
 app.use(express.json());
 
-    //youre code here
+app.get("/", (req, res) => {
+    res.send(helloWorld());
+});
+
+app.get("/hi", (req, res) => {
+    res.send(hi());
+});
 
 app.use(errorMiddleware);
 
---
+++ src/modules/Hi/hi.ts
@@ -0,0 +1,3 @@
+export const hi = (): string => {
+    return "Hi, Devs!";
+}
